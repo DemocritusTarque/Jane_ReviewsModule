e0c8e71c635cfe97a65c410132dfc8f6132cae8c
Jane Lee <sohyunlee53@gmail.com>
2019-01-16 16:32:07 -0800

    Implemented the rest of CRUD operations and tested via Postman

--- server/index.js
+++ server/index.js
@@ -5,24 +5,86 @@ var app = express();
 // var db = require('../database/mongo_index.js');
 var db = require('../database/postgres_index.js');
 var port = 3003;
-var path = require('path');
+// var path = require('path');
 var cors = require('cors');
 
 app.use(cors())
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static('./public/'));
 app.use(/\/\d+\//, express.static('./public/'));
 
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+
 // find all records where productId matches itemID from url
 app.get('/api/item/:id', (req, res) => {
-  let itemID = req.url.slice(10);
+  // let itemID = req.url.slice(10);
+  let itemID = req.params.id;
+
   db.reviews.findAll({
     where: {
       productid: itemID
     }
   })
-  .then((data) => { res.send(data).status(200) })
-  .catch((error) => { res.send('there was error fetching data from db').status(500); });
+  .then((data) => { res.status(200).send(data); })
+  .catch((error) => { res.status(500).send('there was error fetching data from db', error); });
 });
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+// Create a new review for the specified productid
+app.post('/api/item/:id', (req, res) => {
+  let itemID = req.params.id;
+
+  db.reviews.create({
+    productid: itemID,
+    title: req.body.title,
+    username: req.body.username,
+    review: req.body.review,
+    date: req.body.date,
+    stars: req.body.stars,
+    upvotes: req.body.upvotes,
+    downvotes: req.body.downvotes
+  })
+  .then((data) => { res.status(201).send('post success'); })
+  .catch((error) => { res.status(500).send('there was error posting data to db', error); });
+});
+
+// Update one existing review based on specified productid and username
+app.put('/api/item/:id/:username/review', (req, res) => {
+  let itemID = req.params.id;
+  let userID = req.params.username;
+  let val = {
+    title: req.body.title,
+    review: req.body.review,
+    date: req.body.date,
+    stars: req.body.stars,
+    upvotes: req.body.upvotes,
+    downvotes: req.body.downvotes
+  };
+
+  let condition = {
+    where: {
+      productid: itemID,
+      username: userID
+    }
+  };
+
+  db.reviews.update(val, condition)
+  .then((data) => { res.status(202).send('update success'); })
+  .catch((error) => { res.status(500).send('there was error updating data in db', error); });
+});
+
+// Delete one existing review based on specified productid and username
+app.delete('/api/item/:id/:username/review', (req, res) => {
+  let itemID = req.params.id;
+  let userID = req.params.username;
+  
+  db.reviews.destroy({
+    where: {
+      productid: itemID,
+      username: userID
+    }
+  })
+  .then((data) => { res.status(203).send('deleted item'); })
+  .catch((error) => { res.status(500).send('there was error deleting data in db', error); });
+});
